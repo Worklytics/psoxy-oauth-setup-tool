4b3cef4a9dfd29cf4d7ababcadedfd1925ebba62
davidfq <davidfq@gmail.com>
2023-07-05 13:43:12 +0200

    Add support for GitHub
    It also changes prompt's life-cycle: urge end-users to
    create a new OAuth App in the target platform and confirm
    the "Callback URL" registration

--- index.mjs
+++ index.mjs
@@ -1,72 +1,134 @@
 import enquirer from 'enquirer';
+import { printPsoxySetupData } from './lib/utils.mjs';
+import { OAuthJiraCloudHelper } from './lib/oauth-jira-cloud.mjs';
+import { OAuthGitHubHelper } from './lib/oauth-github.mjs';
 import chalk from 'chalk';
-import open from 'open';
-import { JiraCloudHelper } from './lib/oauth.mjs';
+import { URL } from 'url';
 
-const jiraCloudHelper = new JiraCloudHelper();
-jiraCloudHelper.on('authData', (data) => {
-  // TODO improve output and add option to save to file
-  console.log(
-    `\n\nUse these values for your ${chalk.blue(
-      'Worklytics Psoxy configuration'
-    )} (AWS System Manager store / GCP Cloud secrets):`
-  );
-  console.log(`
-    ${chalk.green('PSOXY_JIRA_CLOUD_ACCESS_TOKEN')}: ${data.access_token}
-    ${chalk.green('PSOXY_JIRA_CLOUD_REFRESH_TOKEN')}: ${data.refresh_token}
-    ${chalk.green('PSOXY_JIRA_CLOUD_CLIENT_ID')}: ${data.clientID}
-    ${chalk.green('PSOXY_JIRA_CLOUD_CLIENT_SECRET')}: ${data.clientSecret}
-  `);
-
-  const cloudID = data.cloudIDs.filter(
-    (item) => item.url === data.cloudInstanceURL
-  )[0].id;
-
-  console.log(
-    `Terraform configuration, ${chalk.blue('jira_cloud_id')} = ${cloudID}`
-  );
-
-  jiraCloudHelper.stop();
+const end = (instance) => {
+  instance.stop();
   process.exit(0);
-});
-const oauthRedirectURL = await jiraCloudHelper.start();
+};
 
-const confirmPrompt = new enquirer.Confirm({
-  message: `Do you already have an OAuth 2.0 integration registered in Jira Cloud instance?`,
-});
-const existingApp = await confirmPrompt.run();
+async function processJiraCloud() {
+  const jira = new OAuthJiraCloudHelper();
+  jira.on('psoxysetupdata', (data) => {
+    printPsoxySetupData('JIRA_CLOUD', data);
+  });
+  jira.on('complete', () => end(jira));
 
-const nextStepMessage = existingApp
-  ? `Please, in the "Authorization" section of your application configuration, enter the following value in the ${chalk.bold(
+  const oauthRedirectURL = await jira.start();
+
+  const installAppMessage = `
+    Please, create a new OAuth 2.0 App (3LO) in your Jira Cloud instance (or use and existing one) and enter the following value in the ${chalk.bold(
       'Callback URL'
-    )} field: ${chalk.blue.bold(oauthRedirectURL)}`
-  : // TODO complete instructions based on Psoxy sources documentation
-    `Please register a new OAuth 2.0 integration in your Jira Cloud account`;
-console.log(`${chalk.green('OK')}: ${nextStepMessage}`);
+    )} field: ${chalk.blue.bold(oauthRedirectURL)}
+  `;
+  console.log(`\n${chalk.bold(installAppMessage.trim())}`);
+  const confirmPrompt = new enquirer.Confirm({
+    message: `Have you registered the Callback URL?`,
+  });
 
-const cloudInstanceURL = await new enquirer.Input({
-  message:
-    'Enter the URL of your Jira Cloud instance (e.g. https://mycompany.atlassian.net)',
+  const confirmation = await confirmPrompt.run();
+  if (!confirmation) {
+    end(jira);
+  }
+
+  console.log(
+    `${chalk.green('OK')}: Please, enter the following settings of your App:`
+  );
+  // Enquirer's Snippet could replace these one by one prompts but it
+  // doesn't support "pasting" (bad UX for IDs and secrets)
+  // https://github.com/enquirer/enquirer/issues/165
+  const cloudURLPrompt = new enquirer.Input({
+    message:
+      'URL of your Jira Cloud instance (e.g. https://mycompany.atlassian.net)',
+    required: true,
+    validate(value) {
+      try {
+        new URL(value);
+      } catch (err) {
+        return cloudURLPrompt.styles.danger('cloudURL should be a valid URL');
+      }
+      return true;
+    },
+  });
+  const cloudURL = await cloudURLPrompt.run();
+
+  const clientId = await new enquirer.Input({
+    message: 'Client ID',
+    required: true,
+  }).run();
+
+  // Not using a "password" type prompt since this is intended to be run locally
+  // and the user will get the secret back as output
+  const clientSecret = await new enquirer.Input({
+    message: 'Client Secret',
+    required: true,
+  }).run();
+
+  await jira.authorize({
+    clientId: clientId,
+    clientSecret: clientSecret,
+    cloudURL: cloudURL,
+  });
+}
+
+async function processGitHub() {
+  const github = new OAuthGitHubHelper();
+  github.on('psoxysetupdata', (data) => {
+    printPsoxySetupData('GITHUB', data);
+  });
+  github.on('complete', () => end(github));
+
+  const oauthRedirectURL = await github.start();
+
+  const installAppMessage = `
+    Please, create a new GitHub App in your GitHub's organization (or use and existing one) and enter the following value in the ${chalk.bold(
+      'Callback URL'
+    )} field: ${chalk.blue.bold(oauthRedirectURL)}
+  `;
+  console.log(`\n${chalk.bold(installAppMessage.trim())}`);
+  const confirmPrompt = new enquirer.Confirm({
+    message: `Have you registered the Callback URL?`,
+  });
+
+  const confirmation = await confirmPrompt.run();
+  if (!confirmation) {
+    end(github);
+  }
+
+  console.log(
+    `${chalk.green(
+      'OK'
+    )}: Please, enter the following settings of your GitHub App:`
+  );
+  const clientId = await new enquirer.Input({
+    message: 'Client ID',
+    required: true,
+  }).run();
+
+  const clientSecret = await new enquirer.Input({
+    message: 'Client Secret',
+    required: true,
+  }).run();
+
+  await github.authorize({
+    clientId: clientId,
+    clientSecret: clientSecret,
+  });
+}
+
+const toolChoice = await new enquirer.Select({
+  message: 'Select the tool you want to configure in your Psoxy instance',
+  choices: ['Jira Cloud', 'GitHub'],
 }).run();
 
-const clientID = await new enquirer.Input({
-  message: 'Enter the Client ID',
-}).run();
-
-// Not using a "password" type prompt since this is intended to be run locally
-// and the user will get this value back as output
-const clientSecret = await new enquirer.Input({
-  message: 'Enter the Client Secret',
-}).run();
-
-const authURL = jiraCloudHelper.getAuthURL(
-  clientID,
-  clientSecret,
-  cloudInstanceURL
-);
-
-console.log(
-  `\nOpen this URL in your browser if it doesn't open automatically:\n${authURL}`
-);
-
-await open(authURL, { app: { name: 'google chrome' } });
+switch (toolChoice) {
+  case 'Jira Cloud':
+    processJiraCloud();
+    break;
+  case 'GitHub':
+    processGitHub();
+    break;
+}
